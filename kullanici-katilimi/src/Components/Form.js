22924b5ff872a8a47e342395e9db41730f6c2bd6
alptugozates <omeralptugozates@gmail.com>
2023-11-01 21:46:45 +0300

    Form oluşturuldu

--- kullanici-katilimi/src/Components/Form.js
+++ kullanici-katilimi/src/Components/Form.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 
-const Form = () => {
+const Form = ({ handleSubmit, handleChange, submitDisable, user }) => {
 
 
 
@@ -10,7 +10,27 @@ const Form = () => {
 
 
 
-    return
+    return (
+        <form onSubmit={() => { handleSubmit }}>
+            <label htmlFor="name">İsim Soyisim:
+                <input id="name" type="text" name="name" onChange={handleChange} value={user.name} />
+            </label>
+
+            <label htmlFor="email">E-mail:
+                <input id="email" type="email" name="email" onChange={handleChange} value={user.email} />
+            </label>
+            <label htmlFor="email">Şifre:
+                <input id="password" type="password" name="password" onChange={handleChange} value={user.password} />
+            </label>
+            <label htmlFor="terms">Kayıt sözleşmesini kabul ediyorum.
+                <input id="terms" type="checkbox" name="terms" checked={!user.terms} onChange={handleChange} value={user.terms} />
+            </label>
+
+            <button type="submit">Login</button>
+        </form>
+
+
+    )
 
 
 }
