fffb4f26924d71fbf4d90a5775982ca5144f65ff
alptugozates <omeralptugozates@gmail.com>
2023-11-01 20:28:24 +0300

    Form , changeHandler, onSubmit oluşturuldu

--- kullanici-katilimi/src/App.js
+++ kullanici-katilimi/src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from './Components/Form';
+import axios from "axios";
+import * as Yup from "yup"
+import { useState } from 'react';
 
+
+
+const dummyForm = { name: "", email: "", password: "", terms: false };
+const hataErrors = { name: "", email: "", password: "", terms: "" };
 function App() {
+
+  const [form, setForm] = useState(dummyForm);
+  const [error, setError] = useState(hataErrors);
+
+  const handleSubmit = (e) => {
+    e.PreventDefault();
+    axios
+      .post("https://reqres.in/api/users")
+      .then((res) => setData([...data, res.data]));
+    setForm(dummyForm);
+    setError(hataErrors);
+
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    e.target.type === "checkbox"
+      ? setForm({ ...form, terms: !form.terms })
+      : setForm({ ...form, [name]: value });
+    formCheck(name, value);
+  };
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
---
+++ kullanici-katilimi/src/Components/Form.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+
+
+const Form = () => {
+
+
+
+
+
+
+
+
+    return
+
+
+}
+
+export default Form;
