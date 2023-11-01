7400e71e1be1eb8602045740e1e864baef9a5ee3
alptugozates <omeralptugozates@gmail.com>
2023-11-01 21:12:26 +0300

    formCheck oluşturuldu

--- kullanici-katilimi/src/App.js
+++ kullanici-katilimi/src/App.js
@@ -3,16 +3,29 @@ import './App.css';
 import Form from './Components/Form';
 import axios from "axios";
 import * as Yup from "yup"
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
+const formSchema = Yup.object().shape({
+  name: Yup.string().required("İsim, Soyisim alanının doldurulması zorunludur."),
 
+  email: Yup.string
+    .email("Lütfen geçerli bir email adresi giriniz.")
+    .required("Email adresi boş bırakılamaz."),
+
+  password: Yup.string
+    .password(6, "Minimum 6 karakter içermesi zorunludur.")
+    .required("Şifre alanı boş bırakılamaz."),
+
+  terms: Yup.boolean().oneOf([true], "Kayıt sözleşmesini kabul etmeden işlem gerçekleştirilemez."),
+});
 
 const dummyForm = { name: "", email: "", password: "", terms: false };
 const hataErrors = { name: "", email: "", password: "", terms: "" };
 function App() {
-
+  const [data, setData] = useState([]);
   const [form, setForm] = useState(dummyForm);
   const [error, setError] = useState(hataErrors);
+  const [submitDisabled, setSubmitDisabled] = useState(false);
 
   const handleSubmit = (e) => {
     e.PreventDefault();
@@ -32,13 +45,24 @@ function App() {
     formCheck(name, value);
   };
 
+  const formCheck = (name, value) => {
+    Yup.reach(formSchema, name)
+    validate(value)
+      .then(() => setError({ ...error, [name]: err.errors[0] }));
+  };
+
+  useEffect = (() => {
+    formSchema.isValid(form).then((response) => setSubmitDisabled(response));
+  }, [form])
+
+
 
 
 
 
   return (
     <div className="App">
-
+      <Form handleChange={handleChange} handleSubmit={handleSubmit} submitDisabled={submitDisabled} user={form} />
     </div>
   );
 }
